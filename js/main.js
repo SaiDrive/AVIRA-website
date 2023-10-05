9879e93d96aaf89af7c31ed4497f29ff61abd1fc
fantasticbuddy <riya.1726.verma@gmail.com>
2023-10-06 02:55:33 +0530

    Done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const text = document.querySelector('.text-mask')
 const para = document.querySelector('.text-mask-para')
 window.addEventListener('scroll', function(){
     const value = window.scrollY;
-    // console.log(value);
     text.style.marginTop = value*-0.1 + "px";
     para.style.marginTop = value*-0.1 + "px";
 })
